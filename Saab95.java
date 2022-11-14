0c7d0eee9989f9a9f4fac96ac7921130cd1d1824
Yeenasour <77338618+Yeenasour@users.noreply.github.com>
2022-11-14 11:41:30 +0100

    11-14

--- Saab95.java
+++ Saab95.java
@@ -35,6 +35,6 @@ public class Saab95 extends Supercar {
         setCurrentSpeed(getCurrentSpeed() - speedFactor() * amount);
     }
 
-    @
+    
     
 }
--- Supercar.java
+++ Supercar.java
@@ -1,21 +1,35 @@
 import java.awt.*;
+import java.beans.BeanProperty;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Supercar
  */
-public  abstract class Supercar implements Movable {
+public abstract class Supercar implements Movable {
 
     private int nrDoors;
     private Color color;
     private double enginePower;
     private String modelName;
     private double currentSpeed;
+    private double x;
+    private double y;
+    List<Integer> directionWheel;
 
     public Supercar(int nrDoors, Color color, double enginePower, String modelName) {
         this.nrDoors = nrDoors;
         this.color = color;
         this.enginePower = enginePower;
         this.modelName = modelName;
+        this.x = 0;
+        this.y = 0;
+        this.directionWheel = new ArrayList<Integer>(4);
+        directionWheel.add(1);
+        directionWheel.add(0);
+        directionWheel.add(-1);
+        directionWheel.add(0);
         stopEngine();
     }
 
@@ -32,7 +46,13 @@ public  abstract class Supercar implements Movable {
     }
 
     protected void setCurrentSpeed(double setSpeed) {
-        currentSpeed = setSpeed;
+        if (0 <= setSpeed && setSpeed <= enginePower){
+            System.out.println("yes");
+            currentSpeed = setSpeed;
+        }
+        else {
+            System.out.println("no");
+        }
     }
 
     public Color getColor() {
@@ -59,15 +79,39 @@ public  abstract class Supercar implements Movable {
 
     // TODO fix this method according to lab pm
     public void gas(double amount) {
-        incrementSpeed(amount);
+        if (0 <= amount && amount <= 1){
+            System.out.println("yes");
+            incrementSpeed(amount);
+        }
+        else {
+            System.out.println("no");
+        }
     }
 
     // TODO fix this method according to lab pm
     public void brake(double amount) {
-        decrementSpeed(amount);
+        if (0 <= amount && amount <= 1){
+            System.out.println("yes");
+            decrementSpeed(amount);
+        }
+        else {
+            System.out.println("no");
+        }
     }
 
-    public void move(){
-        
+    @Override
+    public void move() {
+        x += directionWheel.get(1) * getCurrentSpeed();
+        y += directionWheel.get(0) * getCurrentSpeed();
     }
+
+    @Override
+    public void turnLeft(){
+        Collections.rotate(directionWheel, -1);
+    }
+    @Override
+    public void turnRight(){
+        Collections.rotate(directionWheel, 1);
+    }
+
 }
